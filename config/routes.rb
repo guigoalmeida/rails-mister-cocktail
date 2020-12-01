0040ebb647dc8d531091101947d8021951475b21
Guilherme Almeida <guilherme.almeida@enap.gov.br>
2020-12-01 16:26:49 -0300

    working, prior to heroku, css pending adjustments

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # root to: 'cocktails@index'
+  #   get '/cocktails', to: 'cocktails#index'
+  #   get '/cocktails/new', to: 'cocktails#new', as: :new_cocktail
+  #   post '/cocktails', to: 'cocktails#create'
+  #   get '/cocktails/:id/doses/new', to: 'doses#new', as: :new_dose
+  #   post '/cocktails/:id/doses', to: 'doses#create'
+  #   get '/cocktails/:id', to: 'cocktails#show', as: :cocktail
+  # end
+
+  root to: 'cocktails#index'
+  resources :cocktails, only: %i[index show new create] do
+    resources :doses, only: %i[new create]
+    resources :reviews, only: [:create]
+  end
+  resources :doses, only: [:destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: 'lemon')
+Ingredient.create(name: 'ice')
+Ingredient.create(name: 'mint leaves')
+Ingredient.create(name: 'rhum')
+Ingredient.create(name: 'cachaca')
+Ingredient.create(name: 'tequila')
+Ingredient.create(name: 'angostura')
+Ingredient.create(name: 'lemon juice')
+Ingredient.create(name: 'orange')
+Ingredient.create(name: 'orange juice')
+Ingredient.create(name: 'beer')
+Ingredient.create(name: 'whisky')
+Ingredient.create(name: 'gin')
